62e931301c4970eda182dc696760b1b3c8d12d08
iJeane <2168137@slu.edu.ph>
2018-04-13 16:19:37 -0700

    Added smooth scrolling javascript

--- Final Website/js/index.js
+++ Final Website/js/index.js
@@ -1,3 +1,18 @@
+$(document).ready(function(){
+	$('section').addClass('tar');
+	$("a").on('click', function(event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+			$('html, body').animate({
+        		scrollTop: $(hash).offset().top
+      		}, 600, function(){
+      			window.location.hash = hash;
+      		});
+      	}
+    });
+});
+
 $(window).on('scroll', function() {
 	if($(window).scrollTop()) {
 		$('nav').addClass('black');
