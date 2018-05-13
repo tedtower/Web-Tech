29df8c5f6e65f42a99e1eb41702ec300f31fc8df
iJeane <2168137@slu.edu.ph>
2018-05-13 15:59:50 +0800

    Added something unimportant

--- Pinakafinal Na Website/js/index.js
+++ Pinakafinal Na Website/js/index.js
@@ -7,3 +7,13 @@ $(window).on('scroll', function() {
 		$('nav').removeClass('black');
 	}
 })
+
+//Adding background on navbar when clicked
+function addBG() {
+	$('nav').addClass('black');
+}
+
+//Disabling Right Click At The Video
+$('video').bind('contextmenu', function(e) {
+    return false;
+}); 
